c93ce72dad97f67d2b356fdd487b14d62c647832
torbeckjosic985 <torbeckjosic985@gmail.com>
2025-03-17 15:30:58 +0000

    Add 11.ts

---
+++ src/11.ts
@@ -0,0 +1,6 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Usage example
+let randomNumber = getRandomInt(10); // returns a random integer between 0 and 9, inclusive of 0 and exclusive of 10
