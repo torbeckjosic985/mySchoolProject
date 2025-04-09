ee0889dc51910f29f4d0ab01d4f71f4b43d8f701
torbeckjosic985 <torbeckjosic985@gmail.com>
2025-04-09 16:20:54 +0000

    Add 26.ts

---
+++ src/26.ts
@@ -0,0 +1,6 @@
+// Example of a basic function that can be used to perform some operations in TypeScript
+function addNumbers(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(addNumbers(3, 5)); // Output: 8
