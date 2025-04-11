24a54a539fc1ddf65d25b17177001de9718348bd
torbeckjosic985 <torbeckjosic985@gmail.com>
2025-04-11 11:30:53 +0000

    Add 27.ts

---
+++ src/27.ts
@@ -0,0 +1,10 @@
+class MySchoolProject {
+    constructor() {
+        this.name = "My School Project";
+        this.description = "A collection of educational resources and activities for students.";
+    }
+}
+
+// Example usage
+const mySchoolProject = new MySchoolProject();
+console.log(mySchoolProject);
